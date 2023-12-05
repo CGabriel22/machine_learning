75dc1af34428e3910e9be86991fe8173b851d888
cgabriel22 <carlosgabrieldev@gmail.com>
2023-12-04 21:40:35 -0300

    knn

--- knn.py
+++ knn.py
@@ -1,25 +1,49 @@
-import csv
+import pandas as pd
+from decimal import Decimal
 
 def readTable():
   data = []
-  with open('Iris.csv', 'r') as file:
-    reader = csv.reader(file)
-    for line in reader:
-        data.append(line)
+  df = pd.read_csv('./Iris.csv')
+  for i in df.values:
+    data.append(i)
   return data
 
 def mountShapes(data):
-  print("Escolhe os 80% conhecidos")
+  unknownShapeResponse = data[5:150:20]
+  unknownShape = []
+  for i in unknownShapeResponse:
+    unknownShape.append(i[:5])
+  knownShape = data[1:140:15]
+  return knownShape, unknownShape, unknownShapeResponse
 
 def knn(n_neighbors, knownShape, unknownShape ):
-  print("teste")
+  for unknownItem in unknownShape:
+    distances = []
+    for knownItem in knownShape:
+      # print('init------------')
+      v1, v2, v3, v4 = knownItem[1] - unknownItem[1], knownItem[2] - unknownItem[2], knownItem[3] - unknownItem[3], knownItem[4] - unknownItem[4] 
+      # print(v1, v2, v3, v4)
+      q1, q2, q3, q4 = v1**2, v2**2, v3**2, v4**2
+      # print(q1, q2, q3, q4)
+      qSum = q1 + q2 + q3 + q4
+      # print(qSum)
+      euclideanDistance = qSum**(1/2)
+      # print(f'desconhecido: {unknownItem}')
+      # print(f'conhecido: {knownItem}')
+      # print(f'distancia euclideana: {euclideanDistance}')
+      distances.append([euclideanDistance, knownItem[5]])
+    # print(f'---------{unknownItem}----------')
+    # for i in distances:
+    #   print(i)
+    winners = sorted(distances, key=lambda x: x[0])[:n_neighbors]
+     
+    # print(winners)
+    
 
 def main():
   data = readTable()
-  for i in data:
-    print(i)
-  # knownShape, unknownShape = mountShapes(data)
-  # knn(5, knownShape, unknownShape)
+  knownShape, unknownShape, unknownShapeResponse = mountShapes(data)
+  knn(3, knownShape, unknownShape)
 
 if __name__ == "__main__":
   main()
